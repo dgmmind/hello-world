9b256d0f18dd1acad1f448837a8cf11ea27ad117
dgmmind <dgmmind@gmail.com>
2025-07-01 14:45:27 -0600

    update new view

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const server = express();
 server.get('/',(req,res)=>{
     res.send('¡Hola mundo desde Express!')
 });
+server.get('/home',(req,res)=>{
+    res.send('¡Hola vista home!')
+});
 const PORT = 30003;
 
 server.listen(PORT,()=>{
