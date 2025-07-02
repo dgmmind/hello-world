b5bee241706763e1e3f0216edb4b45651bd90f73
dgmmind <dgmmind@gmail.com>
2025-07-01 19:00:27 -0600

    Views Add

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 import express from "express";
+import path from "path";
 
-const server = express();
+const app = express();
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-server.get('/',(req,res)=>{
-    res.send('¡Hola mundo desde Express!')
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/',(req,res)=>{
+    res.render('index', {user:'dgmmind'})
 });
-server.get('/home',(req,res)=>{
-    res.send('¡Hola vista home!')
+app.get('/home',(req,res)=>{
+    res.render('home', {user:'dgmmind'})
 });
 const PORT = 30003;
 
-server.listen(PORT,()=>{
+app.listen(PORT,()=>{
     console.log(`Server activo en: http://localhost:${PORT}`);
 })
